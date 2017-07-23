bee577caf7a1d51994c39f233ba6b80230d4a9d5
Amuraliraju <30381453+Amuraliraju@users.noreply.github.com>
2017-07-23 11:51:38 +0530

    Add files via upload
    java programs

---
+++ ConvertStringToInt.java
@@ -0,0 +1,14 @@
+ //package quiz12;
+
+public class ConvertStringToInt {
+
+	public static void main(String[] args) {
+		// Converting string to int without using parseInt method
+		
+		String s = "777333";
+		int k = Integer.valueOf(s);
+		System.out.println(k);
+	}
+
+}
+
